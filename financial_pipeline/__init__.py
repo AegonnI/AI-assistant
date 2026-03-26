44649e1f1b30a48368744fa1801c6be41069f026
vovchik <volodya.zorin.06@mail.ru>
2026-03-26 09:44:47 +0500

    Очистка ветки: удаление всех файлов

--- financial_pipeline/__init__.py
+++
@@ -1,6 +0,0 @@
-from .financial_pipeline import (
-    format_metrics_for_llm_prompt,
-    run_pipeline_on_uploaded_pdf,
-)
-
-__all__ = ["run_pipeline_on_uploaded_pdf", "format_metrics_for_llm_prompt"]
